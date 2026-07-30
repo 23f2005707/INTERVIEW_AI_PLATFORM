64ca5b4f03e85769d3c97492d2338853a2536f24
Himanshu <23f2005707@ds.study.iitm.ac.in>
2026-07-30 20:21:25 +0530

    Add client and server

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+
+  // serverURL 
+export const ServerUrl = "http://localhost:8000"
+
+const App = () => {
+
+  return (
+   <Routes>
+      <Route path = '/' element = {<Home />} />
+      <Route path = '/auth' element = {<Auth />} />
+   </Routes>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+import connectDb from './config/connectDb.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+import authRouter from './routes/auth.routes.js';
+
+// app initialize
+const app = express()
+
+// conn b/w backad front + end
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))  
+
+const PORT = process.env.PORT || 6000
+
+// middleware 
+app.use(express.json())  // data hmesa json me aaye 
+app.use(cookieParser())  // cookie sahi format me store ho 
+
+
+// routers 
+app.use("/api/auth", authRouter)
+
+// get request 
+app.get('/', (req, res) => {
+    return res.json({message: "server started..."})
+})
+
+
+// server access or runsk
+app.listen(PORT, () => {
+    console.log(`server is running on PORT ${PORT}`)
+    connectDb()
+})
