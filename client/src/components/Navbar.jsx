991dfda361c34c2b3fc7a0405a68f98859d3cd6a
Himanshu <23f2005707@ds.study.iitm.ac.in>
2026-08-02 00:04:15 +0530

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,11 +4,17 @@ import { BsRobot, BsCoin } from "react-icons/bs";
 import { FaUserAstronaut } from "react-icons/fa";
 import { HiOutlineLogout } from "react-icons/hi";
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
 
     // read the data from store 
     const {userData} = useSelector((state) => state.user)
+
+    const [showCreditPopup, setShowCreditPopup] = useState(false)
+    const [showUserPopup, setShowUserPopup] = useState(false)
+    const navigate = useNavigate()
 
   return (
     <div className=' bg-white flex justify-center px-4 pt-6'>
@@ -29,17 +35,60 @@ const Navbar = () => {
         {/* CREDITS POINTS */}
         <div className='flex items-center  gap-6 relative'>
             <div className='relative'>
-                <button className='flex items-center gap-2 bg-gray-100 px-4 py-2 rounded-full text-md hover:bg-gray-200 transition'>
+            {/* CREDITS POPUP ADD */}
+                <button 
+                onClick = {() => {setShowCreditPopup(!showCreditPopup);
+                    setShowUserPopup(false)
+                }}
+                className='flex items-center gap-2 bg-gray-100 px-4 py-2 rounded-full text-md hover:bg-gray-200 transition'>
                     <BsCoin size = {20} />
                     {userData?.credits || 0}
                 </button>
+
+                {showCreditPopup && (
+                    <div className='absolute right-[-50px] mt-3 w-64 bg-white shadow-xl border border-gray-200 rounded p-5 z-50'>
+                    <p className='text-sm text-gray-600 mb-4'>
+                        Need more credits to continue interview?</p>
+                        <button onClick = {() =>navigate("/pricing")}
+                        className='w-full bg-black text-white py-2 rounded-lg text-sm'>
+                            Buy more credits
+                        </button>
+                    </div>
+                    
+                )}
             </div>
 
+            
+            {/* USER FIRST LETTER ICON */}
             <div className='relative'>
-                <button className='w-9 h-9 bg-black text-white rounded-full flex items-center justify-center'>
-                    <BsCoin size = {20} />
-                    {userData?.credits || 0}
+                <button
+                onClick = {() => {setShowUserPopup(!showUserPopup);
+                    setShowCreditPopup(false)
+                }}
+                className='w-9 h-9 bg-black text-white rounded-full flex items-center justify-center font-semibold'>
+                    {userData ? userData?.name.slice(0, 1).toUpperCase() : <FaUserAstronaut size = {16}/>}
                 </button>
+
+
+                {showUserPopup && (
+                    <div className='absolute right-[-50px] mt-3 w-64 bg-white shadow-xl border border-gray-200 rounded p-5 z-50'>
+                        <p className='text-md text-bue-500 font-medium mb-1'>
+                        {userData?.name}
+                        </p>
+
+                        <button 
+                        onClick = {() => navigate("/history")}
+                         className= 'w-full text-left text-sm py-2 hover:text-black text-gray-600'>
+                            Interview History
+                        </button>
+
+                        <button className= 'w-full text-left text-sm py-2 flex items-center gap-2 text-red-500'>
+                            <HiOutlineLogout size = {16}/>
+                            Logout
+                        </button>
+
+                    </div>
+                )}
             </div>
 
 
